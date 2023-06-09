7cab0ff82d1e1fad24c61a0715e81b861ea6ba73
NicholasOden <aoronmaster@gmail.com>
2023-06-10 00:08:20 +0700

    Implementing BottomNavigation

--- app/src/main/java/com/capstone/nempatin/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/nempatin/ui/main/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.capstone.nempatin.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.capstone.nempatin.R
+import com.capstone.nempatin.ui.fragments.FavoriteFragment
+import com.capstone.nempatin.ui.fragments.HomeFragment
+import com.capstone.nempatin.ui.upload.UploadActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.activity_main_bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    replaceFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_add -> {
+                    // Start AddActivity instead of replacing the fragment
+                    val intent = Intent(this, UploadActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.navigation_favorite -> {
+                    replaceFragment(FavoriteFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+
+        // Default selection
+        bottomNavigation.selectedItemId = R.id.navigation_home
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_view, fragment)
+        transaction.commit()
     }
 }
